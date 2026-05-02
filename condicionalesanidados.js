09721c7dd19916882d379c5e4da7d4d4cf06f3ed
Ericson Olaya Castillo <davidolayacs95@gmail.com>
2026-05-01 19:45:30 -0600

    condicionales anidadas

---
+++ condicionalesanidados.js
@@ -0,0 +1,17 @@
+
+const nota = prompt(`Ingrese su nota: `);
+
+
+
+
+    if (nota >= 5) {
+        console.log(`Aprobado, tu nota es: ${nota}`);
+        if (nota >= 9) {
+            console.log(`Excelente, tu nota es: ${nota}`);
+        }
+    } else {
+        console.log(`Reprobado, tu nota es: ${nota}`);
+        if (nota < 3) {
+            console.log(`Muy deficiente, tu nota es: ${nota}`);
+        }       
+    }
