b576dc64f830a4401c434751f596ca3585680409
Ericson Olaya Castillo <davidolayacs95@gmail.com>
2026-05-01 21:19:06 -0600

    || operadores

--- operadores.js
+++ operadores.js
@@ -4,12 +4,30 @@
 
 // 77 Ejemplo para &&
 
- const edad1 = prompt("Ingresa tu edad:");
- const tieneEntrada1 =  confirm("¿Tienes entrada?");
+ //const edad1 = prompt("Ingresa tu edad:");
+ //const tieneEntrada1 =  confirm("¿Tienes entrada?");
 
-if (edad1 >= 18 && tieneEntrada1) {
-    console.log(`Cumples con los requisitos, Tu edad es: ${edad1} años y tienes entrada`);
+//if (edad1 >= 18 && tieneEntrada1) {
+//    console.log(`Cumples con los requisitos, Tu edad es: ${edad1} años y tienes entrada`);
+//} else {
+//    console.log(`No cumples con los requisitos, Tu edad es: ${edad1} años  y ${tieneEntrada1 ? "tienes entrada" : "no tienes entrada"}  `);
+//}
+
+
+// ejemplo para || 
+
+const tieneEfectivo = true;
+const tieneTarjeta = true;
+const ninguno = false;
+
+
+
+const pago = prompt("¿Cómo deseas pagar? (efectivo/tarjeta/ninguno)");
+
+const puedeComprar = (pago === "efectivo" && tieneEfectivo) || (pago === "tarjeta" && tieneTarjeta) || (pago === "ninguno" && ninguno);
+
+if (puedeComprar) {
+    console.log("Puedes realizar la compra.");
 } else {
-    console.log(`No cumples con los requisitos, Tu edad es: ${edad1} años  y ${tieneEntrada1 ? "tienes entrada" : "no tienes entrada"}  `);
-}
-
+    console.log("No puedes realizar la compra, no tienes efectivo o tarjeta.");
+}   
