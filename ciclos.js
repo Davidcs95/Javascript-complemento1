31c51ae4ad37753a2c95bbefb89333b690fe43c9
Ericson Olaya Castillo <davidolayacs95@gmail.com>
2026-05-03 20:18:42 -0600

    ciclos

---
+++ ciclos.js
@@ -0,0 +1,21 @@
+// El ciclo for (Tradicional)
+//const precios = [10000, 20000, 30000, 40000];
+
+//console.log("Aplicando descuentos:");
+
+// i parte en 0, llega hasta el final del array (.length) y aumenta de 1 en 1
+//for (let i = 0; i < precios.length; i++) {
+  //  let descuento = precios[i] * 0.9; // 10% de descuento
+   // console.log(`Producto ${i + 1}: El nuevo precio es $${descuento}`);
+//}
+
+// 2. El ciclo forEach
+
+//const mascotas = ["Perro", "Gato", "Ave", "Pez"];
+
+//console.log("Inventario de mascotas:");
+
+// mascota representa a cada elemento individual mientras el ciclo avanza
+//mascotas.forEach((mascota, indice) => {
+  //  console.log(`${indice + 1}. Tipo de animal: ${mascota}`);
+//});
