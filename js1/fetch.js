1f2720c7d7c70f6f4f2a277408b963a26c6d3837
Ericson Olaya Castillo <davidolayacs95@gmail.com>
2026-05-10 19:10:32 -0600

    fetch

---
+++ js1/fetch.js
@@ -0,0 +1,12 @@
+//  función fetch
+
+// Para el ejemplo vamos a dejar la funcion miblob. La cual sirve para ctraer img de otras paginas
+
+// fetch('https://google.com')
+// .then(respuesta => respuesta.blob())
+// .then(miBlob => {
+//  const rutaImagen = URL.createObjectURL(miBlob);
+//  document.getElementById('logo-google').src = rutaImagen;
+// console.log("¡Imagen cargada con éxito!");
+//  })
+//   .catch(error => console.error("No pudimos traer el logo:", error));
