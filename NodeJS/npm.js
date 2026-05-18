e0ec22df6198017ebbe8716b85c5d60f7f087f30
Ericson Olaya Castillo <davidolayacs95@gmail.com>
2026-05-17 18:39:23 -0600

    What is a NPM and how it work

---
+++ NodeJS/npm.js
@@ -0,0 +1,13 @@
+// npm es una herramienta incluida con Node.js que sirve para administrar paquetes y dependencias en proyectos de JavaScript.
+// ¿Para qué sirve? Con npm puedes:
+// - Instalar librerías.
+// - Crear proyectos.
+// - Actualizar paquetes.
+// - Ejecutar scripts.
+// - Administrar dependencias.
+
+// Ejemplo 
+// mkdir mi-proyecto
+// cd mi-proyecto
+// npm init -y
+// npm install express
